394426853dae4fa3ae58da7fd5f86e290e3c13ce
Ratul Hossain <iamratul93@gmail.com>
2023-05-07 04:50:47 +0600

    Upload new project

---
+++ assignment12/assets/js/main.js
@@ -0,0 +1,36 @@
+// Element
+const calculateBtn = document.getElementById('calculateBtn');
+const weightInput = document.getElementById('weightInput');
+const heightInput = document.getElementById('heightInput');
+const result = document.getElementById('result');
+const bmiMessage = document.getElementById('bmiMessage');
+
+const bmiResult = () => {
+    // Get Data
+    const bmiWeightData = weightInput.value;
+    const bmiWeight = parseFloat(bmiWeightData * 2.20462);
+    // weightInput.value='';
+
+    const bmiheightData = heightInput.value;
+    const bmiheight = parseFloat(bmiheightData);
+    const squareHeight = bmiheight * bmiheight;
+    // heightInput.value='';
+
+    // Calculating Data
+    const bmiCalculate = (bmiWeight / squareHeight) * 703;
+    result.innerText = bmiCalculate.toFixed(1);
+
+    // Display Message
+    // const message = '';
+    if(bmiCalculate<18.5){
+        bmiMessage.innerHTML = '<div class="alert alert-warning" role="alert">Opps! You are <b>Underweight!</b></div>';
+    } else if(bmiCalculate>18.5 && bmiCalculate<=24.9){
+        bmiMessage.innerHTML = '<div class="alert alert-success" role="alert">Wow! Your weight is <b>Normal</b>.</div>';
+    } else if(bmiCalculate>24.9 && bmiCalculate<=29.9){
+        bmiMessage.innerHTML = '<div class="alert alert-warning" role="alert">Opps! You are <b>Overweight</b>.</div>';
+    } else if(bmiCalculate>29.9){
+        bmiMessage.innerHTML = '<div class="alert alert-danger" role="alert">You need to contact a nutritionist immediately.</div>';
+    }
+    // bmiMessage.innerText = message;
+}
+calculateBtn.addEventListener('click', bmiResult);
